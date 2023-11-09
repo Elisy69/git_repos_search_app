319bc1c66fb82f14ed334aebcf15715b5592c4ca
Elisei <strashevsky.elisey@gmail.com>
2023-11-09 14:20:18 +0300

    app initiated

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import SearchBar from "@components/searchBar/SearchBar.vue";
+</script>
+
+<template>
+  <SearchBar />
+</template>
---
+++ src/components/searchBar/SearchBar.vue
@@ -0,0 +1,3 @@
+<script lang="ts" setup></script>
+
+<template><input type="text" /> <button>search</button></template>
